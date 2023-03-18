89a70215ab7234ffe7a89303cc0fc782ebd59ca2
incava <ingi1118@naver.com>
2023-03-18 16:17:28 +0900

    feat : 뷰페이저 구현

--- app/src/main/java/com/incava/notyourfaultkotlin/MainActivity.kt
+++ app/src/main/java/com/incava/notyourfaultkotlin/MainActivity.kt
@@ -3,14 +3,39 @@ package com.incava.notyourfaultkotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayoutMediator
 import com.incava.notyourfaultkotlin.databinding.ActivityMainBinding
+
+/**
+ * 뷰페이저로 map과 Query를 보여주기 위한 액티비티.
+ */
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    private lateinit var viewPagerAdapter: ViewPagerAdapter
+    private var fragmentArr = arrayListOf (MapFragment(),QueryFragment(),WantFragment())
+    private var tabArr = arrayListOf ("Map","조회","찜")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setViewPager()
+        setTabLayout()
+
+    }
+    private fun setViewPager(){ // 뷰페이저 설정
+        viewPagerAdapter = ViewPagerAdapter(this@MainActivity)
+        viewPagerAdapter.addAllFragment(fragmentArr)
+        binding.pager.apply {
+            adapter = viewPagerAdapter
+        }
+    }
+    private fun setTabLayout(){ // 탭레이아웃 설정
+        TabLayoutMediator(binding.tabLayout, binding.pager) {tab, position ->
+            tab.text = tabArr[position]
+        }.attach()
     }
 
 
