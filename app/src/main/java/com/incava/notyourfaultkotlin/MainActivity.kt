e64e97e24e83dd3381281a310a8d5bff9da159c0
incava <ingi1118@naver.com>
2023-03-20 16:27:42 +0900

    fix : 뷰페이저와 지도의 제스처 겹침현상으로 바텀네비게이션으로 변경

--- app/src/main/java/com/incava/notyourfaultkotlin/MainActivity.kt
+++ app/src/main/java/com/incava/notyourfaultkotlin/MainActivity.kt
@@ -7,9 +7,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.TableLayout
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.Tab
 import com.google.android.material.tabs.TabLayoutMediator
 import com.incava.notyourfaultkotlin.data.Item
 import com.incava.notyourfaultkotlin.data.ShelterDTO
@@ -24,32 +32,19 @@ import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var viewPagerAdapter: ViewPagerAdapter
-    private var fragmentArr = arrayListOf(MapFragment(), QueryFragment(), WantFragment())
-    private var tabArr = arrayListOf("Map", "조회", "찜")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setViewPager()
-        setTabLayout()
-
+        setNavigation()
+    }
+    private fun setNavigation(){
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fcv) as NavHostFragment
+        val navController = navHostFragment.findNavController()
+        binding.bottomNavigationView.setupWithNavController(navController) //바텀네비게이션과 프래그먼트를 연결시켜 select시 이동하도록 구현.
+        //menu의 아이템 id와 navigation id는 같아야 자동으로 연결된다.
     }
 
-
-    private fun setViewPager() { // 뷰페이저 설정
-        viewPagerAdapter = ViewPagerAdapter(this@MainActivity)
-        viewPagerAdapter.addAllFragment(fragmentArr)
-        binding.pager.apply {
-            adapter = viewPagerAdapter
-        }
-    }
-
-    private fun setTabLayout() { // 탭레이아웃 설정
-        TabLayoutMediator(binding.tabLayout, binding.pager) { tab, position ->
-            tab.text = tabArr[position]
-        }.attach()
-    }
 
     override fun onDestroy() {
         super.onDestroy()
